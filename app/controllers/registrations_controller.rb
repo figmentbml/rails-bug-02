778d7a0753986a27f78d9ab9a399a3329460c934
figmentbml <beth.jaswa@gmail.com>
2014-11-07 12:07:03 -0700

    wrote test and fixed bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,8 +9,8 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation, :name))
     if @user.save
-      session[:user] = @user.id
-      redirect_to root_path
+      session[:user_id] = @user.id
+      redirect_to root_path(session[:user])
     else
       render :new
     end
--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,16 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'User registers and auto logs in' do
+    visit root_path
+    click_on "Register"
+    fill_in "Name", with: "Bob"
+    fill_in "Email", with: "bob@example.com"
+    fill_in "Password", with: "123"
+    fill_in "Confirm", with: "123"
+    click_button "Register"
+    expect(page).to have_content("bob@example.com")
+    expect(page).to have_no_content("Login")
+  end
+
 end
